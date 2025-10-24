895b83804fb41d0ab00c50bae44b55d54560b5f4
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-25 00:39:38 +0530

    Delete src/VerifyEmail.jsx

--- src/VerifyEmail.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect } from 'react';
-export default function VerifyEmail(){
-  useEffect(()=>{ alert('If you clicked the email verify link, the backend will mark your email verified.'); },[]);
-  return <div>Follow the link in the email you received to verify your address. Then return to the app.</div>;
-}
